833b7092ca2e7062fdb551efc7ebeb411dde5426
Simon Dunn <simondunn90@gmail.com>
2021-03-06 17:53:48 +0000

    basic styling and post request

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/login', methods=["POST"])
+def receive_data():
+    username = request.form["username"]
+    password = request.form["password"]
+    return f"You logged in with the username {username} and password {password}"
+
 
 
 if __name__ == "__main__":
