3c5f6ed2f77c1f8269b38938c6fd943b1546d3ed
Simon Dunn <simondunn90@gmail.com>
2021-03-11 17:11:16 +0000

    Added functionality to contact form with smtplib

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 from flask import Flask, render_template, request
-app = Flask(__name__)
+import smtplib
 
+
+def send_email(name, email, message):
+    my_email = "appdunntest@gmail.com"
+    password = "password goes here"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user= my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs=my_email,
+                            msg=f"Subject: Contact form \n\n Name: {name} \n Email: {email} \n Message: {message}")
+
+
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
-@app.route('/login', methods=["POST"])
-def receive_data():
-    username = request.form["username"]
-    password = request.form["password"]
-    return f"You logged in with the username {username} and password {password}"
 
+@app.route('/contact', methods=["POST"])
+def contact():
+    if request.method == "POST":
+        name = request.form["contact_name"]
+        email = request.form["contact_email"]
+        message = request.form["contact_message"]
+        send_email(name, email, message)
+        return render_template('index.html', msg_sent=True)
+    return render_template('index.html', msg_sent=False)
 
 
 if __name__ == "__main__":
